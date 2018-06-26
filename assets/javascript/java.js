86c4ecee6d1144dde515d0745afb5bf6520aec8e
Annie Vallone <annievallone@gmail.com>
2018-06-26 18:01:39 -0400

    hw

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -13,32 +13,59 @@ var a = $("<button>");
 }
 }
 
-$(".giph-btn").on("click", function(event) {
+$(document).on("click", ".giph-btn", function() {
   event.preventDefault();
   var thisGiph = $(this).attr("data-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  thisGiph + "&api_key=CGW52nQLOT4fGclXAdJ84QtgK5ZzjYyO&limit=5";
+  thisGiph + "&api_key=CGW52nQLOT4fGclXAdJ84QtgK5ZzjYyO&limit=10";
   
 
       $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        var rating = response.rating;
+      for (var i = 0; i < response.data.length; i++){
+        var rating = response.data[i].rating;
         var giphDiv = $("<div class='giph'>")
         var ratingDiv = $("<p>").text("Rating: " + rating);
         giphDiv.append(ratingDiv)
+        var giphImage = $("<img class='gif'>")
+        giphImage.attr("src", response.data[i].images.fixed_height_still.url)
+        giphImage.attr("data-still", response.data[i].images.fixed_height_still.url)
+        giphImage.attr("data-animate", response.data[i].images.fixed_height.url)
+        giphImage.attr("data-state", "still")
+        giphDiv.append(giphImage)
         $(".giphy").prepend(giphDiv);
       console.log(response);
+      }
+      
+        
     });
 });
   
- 
   $("#add-giphy").on("click", function(event) {
         event.preventDefault();
-        var newGiphy = $(".btn").val().trim();
+        var newGiphy = $("#giphy-input").val().trim();
         giphy.push(newGiphy)
         showbtns();
+        document.getElementById("giphy-input").value = "";
+
+      });
+      
+      $(document).on("click", ".gif", function() {
+        // event.preventDefault();
+        
+        var state = $(this).attr("data-state");
+        console.log("state: " + state);
+        
+        if(state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+        
       });
       
       showbtns();
